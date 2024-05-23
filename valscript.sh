f908f2f43934deaa07cdb8b793702d31f1c90f10
Eli Joshua <ejtmaravillas@gmail.com>
2024-05-23 08:20:30 +0000

    fixed minor issues with valscript.sh

--- valscript.sh
+++ valscript.sh
@@ -1,1 +1,1 @@
-valgrind --suppressions=valgrind_leaks_ignore.txt --leak-check=full --show-leak-kinds=all --track-origins=yes --malloc-fil=0x41 --free-fill=0x42 --track-fds=yes --malloc-fill=0x41 --free-fill=0x42 ./minishell\
+valgrind --suppressions=valgrind_leaks_ignore.txt --leak-check=full --show-leak-kinds=all --track-origins=yes --malloc-fill=0x41 --free-fill=0x42 --track-fds=yes --malloc-fill=0x41 --free-fill=0x42 ./minishell\
